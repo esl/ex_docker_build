7ee3e359de6cffabb726f6531be3427fce342e36
sescobb27 <s.escob.b.27@gmail.com>
2018-09-27 08:04:46 -0500

    [add] coveralls and mock dependencies for testing

--- mix.exs
+++ mix.exs
@@ -11,7 +11,14 @@ defmodule ExDockerBuild.MixProject do
       lockfile: "mix.lock",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -26,7 +33,10 @@ defmodule ExDockerBuild.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.3"},
-      {:poison, "~> 4.0"}
+      {:poison, "~> 4.0"},
+      {:excoveralls, "~> 0.9", only: :test},
+      {:dialyxir, "~> 1.0.0-rc.3", only: [:dev], runtime: false},
+      {:mock, "~> 0.3.2", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true},
