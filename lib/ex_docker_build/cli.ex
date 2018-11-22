1b701bb499a2b59ed21e52d5668166aed00ecb46
Filipe Varjão <varjaofilipe@gmail.com>
2018-11-22 14:54:35 -0300

    removing unnecessary IO.inspect

--- lib/ex_docker_build/cli.ex
+++ lib/ex_docker_build/cli.ex
@@ -1,14 +1,13 @@
 defmodule ExDockerBuild.CLI do
   def main([]) do
-    IO.puts("You forgot to pass the path to the Dockerfile")
+    IO.puts "You forgot to pass the path to the Dockerfile"
   end
 
-  def main(args) do
-    IO.inspect(args)
+  def main([args | _]) do
     path = Path.expand(args)
 
     {:ok, _} =
-      Path.join([path, "Dockerfile"])
+      path
       |> ExDockerBuild.DockerfileParser.parse_file!()
       |> ExDockerBuild.DockerBuild.build(path)
   end
