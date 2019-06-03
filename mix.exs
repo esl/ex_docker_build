05d66015799631fb8876f20ae8c30aea1d234e2b
bryan <bryan.hunt@erlang-solutions.com>
2019-06-03 09:49:39 +0100

    profiling tools for dev/test

--- mix.exs
+++ mix.exs
@@ -39,10 +39,11 @@ defmodule ExDockerBuild.MixProject do
       {:excoveralls, "~> 0.9", only: :test},
       {:dialyxir, "~> 1.0.0-rc.3", only: [:dev], runtime: false},
       {:mock, "~> 0.3.2", only: :test},
-      {:ex_doc, ">= 0.0.0", only: :dev}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true},
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:benchee, ">= 1.0.0", only: [:dev, :test]},
+      {:rexbug, ">= 1.0.0", only: [:dev, :test]},
+      # tracer hex.pm package doesn't include flamegraph scripts..
+      {:tracer, git: "git@github.com:gabiz/tracer.git", branch: "master", only: [:dev, :test]},
     ]
   end
 
